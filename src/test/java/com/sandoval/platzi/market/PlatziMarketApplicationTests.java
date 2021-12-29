fdc7e02e1d3bd1b755cec555de87a0dbb1c24a58
Carlos Sandoval <carlos_desarrollo@tecnologiasis.com>
2021-12-29 16:39:24 -0600

    complete project structure 2.0

---
+++ src/test/java/com/sandoval/platzi/market/PlatziMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandoval.platzi.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
